35b151375bf0a830d7d2874fd48f132825f60959
bug1s <162743587+bgszk@users.noreply.github.com>
2026-03-27 21:54:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ btn.addEventListener('click', () => {
   audio.play();
 });
 
-// FUNDO ANIMADO PROFESSIONAL (Three.js)
+// FUNDO ANIMADO (Three.js)
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('background'), alpha: true });
